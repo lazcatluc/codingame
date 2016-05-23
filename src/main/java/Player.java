32a5f1fca649fe4184b4c31bb47d877bd1112dc2
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-23 15:35:26 +0300

    initial commit

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -13,103 +13,24 @@ import java.util.Set;
  **/
 class Player {
 
-	static final Set<Node> GATEWAYS = new LinkedHashSet<>();
-	
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int numberOfNodes = in.nextInt(); 
-        int numberOfLinks = in.nextInt(); 
-        int numberOfGateways = in.nextInt(); 
-        for (int i = 0; i < numberOfLinks; i++) {
-            int firstNode = in.nextInt(); // N1 and N2 defines a link between these nodes
-            int secondNode = in.nextInt();
-            Node.get(firstNode).addLink(Node.get(secondNode));
-        }
-        for (int i = 0; i < numberOfGateways; i++) {
-            markGateway(in.nextInt()); // the index of a gateway node
-        }
+    	Scanner in = new Scanner(System.in);
+        int W = in.nextInt(); // width of the building.
+        int H = in.nextInt(); // height of the building.
+        int N = in.nextInt(); // maximum number of turns before game over.
+        int X0 = in.nextInt();
+        int Y0 = in.nextInt();
 
         // game loop
         while (true) {
-            int skynetAgentIndex = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
-            
-            List<Node> shortestPath = findShortestPathToGatewaysFrom(skynetAgentIndex);
-            
-            System.out.println(printLink(shortestPath.get(0), shortestPath.get(1)));
+            String bombDir = in.next(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+
+            // the location of the next window Batman should jump to.
+            System.out.println("0 0");
         }
     }
-    
-    static class Node {
-    	private final int index;
-    	private final Set<Node> linked = new LinkedHashSet<>();
-    	private static final Map<Integer, Node> NODES = new HashMap<>();
-    	
-    	private Node(int index) {
-    		this.index = index;
-    	}
-    	
-    	public void addLink(Node node) {
-    		linked.add(node);
-    		node.linked.add(this);
-    	}
-    	
-    	public static Node get(int index) {
-    		Node node = NODES.get(index);
-    		if (node == null) {
-    			node = new Node(index);
-    			NODES.put(index, node);
-    		}
-    		
-    		return node;
-    	}
-
-		public Set<Node> getLinks() {
-			return linked;
-		}
-
-		public void severLink(Node node) {
-			linked.remove(node);
-			node.linked.remove(this);
-		}
-
-		@Override
-		public String toString() {
-			return String.valueOf(index);
-		}
-		
-    }
-
-	public static void severLinkBetween(int i, int j) {
-		Node.get(i).severLink(Node.get(j));
-		Node.get(j).severLink(Node.get(i));
-	}
-
-	public static void markGateway(int index) {
-		GATEWAYS.add(Node.get(index));
-	}
-
-	public static Set<List<Node>> findPathsToGatewaysFrom(int startingPoint) {
-		return findPathsToGatewaysFrom(Node.get(startingPoint), Collections.emptyList());
-		
-	}
-	
-	private static Set<List<Node>> findPathsToGatewaysFrom(Node start, List<Node> previousNodes) {
-		List<Node> completeNodes = new ArrayList<>(previousNodes);
-		completeNodes.add(start);
-		if (GATEWAYS.contains(start)) {
-			return Collections.singleton(completeNodes);
-		}
-		Set<List<Node>> ret = new LinkedHashSet<>();
-		start.getLinks().stream().filter(node -> !previousNodes.contains(node))
-				.map(node -> findPathsToGatewaysFrom(node, completeNodes)).forEach(ret::addAll);
-		return ret;
-	}
-
-	public static List<Node> findShortestPathToGatewaysFrom(int startingPoint) {
-		return findPathsToGatewaysFrom(startingPoint).stream().min((path1, path2) -> path1.size() - path2.size()).get();
-	}
-
-	public static String printLink(Node start, Node finish) {
-		return start.toString() + " " + finish.toString();
-	}
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,63 +1,8 @@
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
-
-import java.util.Arrays;
-import java.util.LinkedHashSet;
 
 import org.junit.Test;
 
 public class PlayerTest {
-	@Test
-	public void addingALinkAddsTheOppositeLink() throws Exception {
-		Player.Node.get(0).addLink(Player.Node.get(1));
-		
-		assertThat(Player.Node.get(1).getLinks()).contains(Player.Node.get(0));
-	}
 	
-	@Test
-	public void severingALinkResultsInLinkBeingRemoved() throws Exception {
-		Player.Node.get(0).addLink(Player.Node.get(1));
-		
-		Player.severLinkBetween(0, 1);
-		
-		assertThat(Player.Node.get(1).getLinks()).doesNotContain(Player.Node.get(0));
-	}
-	
-	@Test
-	public void generatesPathsFrom0ToGateway() throws Exception {
-		Player.Node.get(0).addLink(Player.Node.get(1));
-		Player.Node.get(1).addLink(Player.Node.get(2));
-		Player.Node.get(1).addLink(Player.Node.get(3));
-		
-		Player.markGateway(2);
-		Player.markGateway(3);
-		
-		assertThat(Player.findPathsToGatewaysFrom(0)).isEqualTo(new LinkedHashSet<>(Arrays.asList(Arrays.asList(
-				Player.Node.get(0), Player.Node.get(1), Player.Node.get(2)),Arrays.asList(
-						Player.Node.get(0), Player.Node.get(1), Player.Node.get(3)))));
-		
-		Player.GATEWAYS.clear();
-	}
-	
-	@Test
-	public void getsShortestPathToGateway() throws Exception {
-		Player.Node.get(0).addLink(Player.Node.get(1));
-		Player.Node.get(1).addLink(Player.Node.get(2));
-		Player.Node.get(1).addLink(Player.Node.get(3));
-		Player.Node.get(3).addLink(Player.Node.get(4));
-		
-		Player.markGateway(2);
-		Player.markGateway(4);
-		
-		assertThat(Player.findShortestPathToGatewaysFrom(0)).isEqualTo(Arrays.asList(
-				Player.Node.get(0), Player.Node.get(1), Player.Node.get(2)));
-	}
-	
-	@Test
-	public void printsLinkBetween0And1() throws Exception {
-		Player.Node.get(0).addLink(Player.Node.get(1));
-		
-		assertThat(Player.printLink(Player.Node.get(0), Player.Node.get(1))).isEqualTo("0 1");
-	}
 }

fc5b95419b1625343cd1d9baabfffa4d474d3087
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-24 14:41:43 +0300

    complete battle logic

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -11,12 +14,14 @@ class Player {
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt(); // the number of cards for player 1
+		Queue<Card> player1 = new LinkedList<>();
 		for (int i = 0; i < n; i++) {
-			String cardp1 = in.next(); // the n cards of player 1
+			player1.offer(getCard(in.next())); // the n cards of player 1
 		}
 		int m = in.nextInt(); // the number of cards for player 2
+		Queue<Card> player2 = new LinkedList<>();
 		for (int i = 0; i < m; i++) {
-			String cardp2 = in.next(); // the m cards of player 2
+			player2.offer(getCard(in.next())); // the m cards of player 2
 		}
 
 		// Write an action using System.out.println()
@@ -24,8 +29,99 @@ class Player {
 
 		System.out.println("PAT");
 	}
+	
+	static class Battle {
+		private final Queue<Card> player1;
+		private final Queue<Card> player2;
+		private final Queue<Card> player1PreviousBattleCards;
+		private final Queue<Card> player2PreviousBattleCards;
+		private final Card player1BattleCard;
+		private final Card player2BattleCard;
+		private final int round;
+		
+		public Battle(Collection<Card> player1, Collection<Card> player2) {
+			this(player1, player2, new LinkedList<>(), new LinkedList<>(), 0);
+		}
+		
+		private Battle(Collection<Card> player1, Collection<Card> player2, Collection<Card> player1PreviousBattleCards,
+				Collection<Card> player2PreviousBattleCards, int previousRound) {
+			this.player1 = new LinkedList<>(player1);
+			this.player2 = new LinkedList<>(player2);
+			this.player1PreviousBattleCards = new LinkedList<>(player1PreviousBattleCards);
+			this.player2PreviousBattleCards = new LinkedList<>(player2PreviousBattleCards);
+			player1BattleCard = this.player1.poll();
+			this.player1PreviousBattleCards.offer(player1BattleCard);
+			player2BattleCard = this.player2.poll();
+			this.player2PreviousBattleCards.offer(player2BattleCard);
+			this.round = previousRound + 1;
+		}
+		
+		public Result getResult() {
+			if (player1BattleCard == null || player2BattleCard == null) {
+				return pat();
+			}
+			int cardsBattleScore = new CardComparator().compare(player1BattleCard, player2BattleCard);
+			if (cardsBattleScore == 0) {
+				if (moveWarCards(player1, player1PreviousBattleCards) &&
+					moveWarCards(player2, player2PreviousBattleCards)) {
+					return new Battle(player1, player2, player1PreviousBattleCards,
+							player2PreviousBattleCards, round - 1).getResult();
+				}
+				return pat();
+			}
+			if (cardsBattleScore < 0) {
+				if (player1.isEmpty()) {
+					return new Result("2 "+round);
+				}
+				player2.addAll(player1PreviousBattleCards);
+				player2.addAll(player2PreviousBattleCards);
+				
+			}
+			if (cardsBattleScore > 0) {
+				if (player2.isEmpty()) {
+					return new Result("1 "+round);
+				}
+				player1.addAll(player1PreviousBattleCards);
+				player1.addAll(player2PreviousBattleCards);
+			}
+			player1PreviousBattleCards.clear();
+			player2PreviousBattleCards.clear();
+			
+			return new Battle(player1, player2, player1PreviousBattleCards,
+					player2PreviousBattleCards, round).getResult();
+		}
 
-	static Card get(String description) {
+
+		private boolean moveWarCards(Queue<Card> player, Queue<Card> playerPreviousBattleCards) {
+			for (int i = 0; i < 3; i++) {
+				Card card = player.poll();
+				if (card == null) {
+					return false;
+				}
+				playerPreviousBattleCards.add(card);
+			}
+			return true;
+		}
+		
+		private static Result pat() {
+			return new Result("PAT");
+		}
+	}
+	
+	static class Result {
+		private final String result;
+
+		public Result(String result) {
+			this.result = result;
+		}
+
+		@Override
+		public String toString() {
+			return result;
+		}
+	}
+
+	static Card getCard(String description) {
 		int lastCharacterIndex = description.length() - 1;
 		Card.Suit suit = getEnumFromFirstChar(Card.Suit.class, description.charAt(lastCharacterIndex));
 		
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,5 +1,8 @@
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -7,36 +10,75 @@ public class PlayerTest {
 	private final Player.CardComparator cc = new Player.CardComparator();
 	@Test
 	public void lowCardLessThanHighCard() throws Exception {
-		assertThat(cc.compare(Player.get("10H"), Player.get("AS"))).isLessThan(0);
+		assertThat(cc.compare(Player.getCard("10H"), Player.getCard("AS"))).isLessThan(0);
 	}
 	
 	@Test
 	public void highCardGreaterThanLowCard() throws Exception {
-		assertThat(cc.compare(Player.get("KD"), Player.get("2C"))).isGreaterThan(0);
+		assertThat(cc.compare(Player.getCard("KD"), Player.getCard("2C"))).isGreaterThan(0);
 	}
 	
 	@Test
 	public void lowCardLessThanLowCard() throws Exception {
-		assertThat(cc.compare(Player.get("9H"), Player.get("10C"))).isLessThan(0);
+		assertThat(cc.compare(Player.getCard("9H"), Player.getCard("10C"))).isLessThan(0);
 	}
 	
 	@Test
 	public void lowCardGreaterThanLowCard() throws Exception {
-		assertThat(cc.compare(Player.get("9H"), Player.get("2D"))).isGreaterThan(0);
+		assertThat(cc.compare(Player.getCard("9H"), Player.getCard("2D"))).isGreaterThan(0);
 	}
 	
 	@Test
 	public void highCardGreaterThanHighCard() throws Exception {
-		assertThat(cc.compare(Player.get("KD"), Player.get("JC"))).isGreaterThan(0);
+		assertThat(cc.compare(Player.getCard("KD"), Player.getCard("JC"))).isGreaterThan(0);
 	}
 	
 	@Test
 	public void lowCardsEqual() throws Exception {
-		assertThat(cc.compare(Player.get("9H"), Player.get("9D"))).isEqualTo(0);
+		assertThat(cc.compare(Player.getCard("9H"), Player.getCard("9D"))).isEqualTo(0);
 	}
 	
 	@Test
 	public void highCardsEqual() throws Exception {
-		assertThat(cc.compare(Player.get("QS"), Player.get("QS"))).isEqualTo(0);
+		assertThat(cc.compare(Player.getCard("QS"), Player.getCard("QS"))).isEqualTo(0);
+	}
+	
+	@Test
+	public void player1WinsFastTest() throws Exception {
+		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D")), 
+							Arrays.asList(Player.getCard("9D"))).getResult().toString()).isEqualTo("1 1");
+	}
+	
+	@Test
+	public void fastPat() throws Exception {
+		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D")), 
+							Arrays.asList(Player.getCard("10C"))).getResult().toString()).isEqualTo("PAT");
+	}
+	
+	@Test
+	public void player2WinsFastTest() throws Exception {
+		assertThat(new Player.Battle(Arrays.asList(Player.getCard("2D")), 
+							Arrays.asList(Player.getCard("9D"))).getResult().toString()).isEqualTo("2 1");
+	}
+	
+	@Test
+	public void player1WinsTwoRoundsTest() throws Exception {
+		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D"), Player.getCard("KC")), 
+							Arrays.asList(Player.getCard("9D"), Player.getCard("2D"))).getResult().toString())
+				.isEqualTo("1 2");
+	}
+	
+	@Test
+	public void complexPat() throws Exception {
+		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D"), Player.getCard("9S"), Player.getCard("8D"), Player.getCard("KH"), Player.getCard("7D"), Player.getCard("5H"), Player.getCard("6S")), 
+							Arrays.asList(Player.getCard("10H"), Player.getCard("7H"), Player.getCard("5C"), Player.getCard("QC"), Player.getCard("2C"), Player.getCard("4H"), Player.getCard("6D")))
+				.getResult().toString()).isEqualTo("PAT");
+	}
+	
+	@Test
+	public void complexPlayer1Wins() throws Exception {
+		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D"), Player.getCard("9S"), Player.getCard("8D"), Player.getCard("KH"), Player.getCard("7D"), Player.getCard("5H"), Player.getCard("7S")), 
+							Arrays.asList(Player.getCard("10H"), Player.getCard("7H"), Player.getCard("5C"), Player.getCard("QC"), Player.getCard("2C"), Player.getCard("4H"), Player.getCard("6D")))
+				.getResult().toString()).isEqualTo("1 3");
 	}
 }

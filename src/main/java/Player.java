1c97e3259412fb6b042e948fc004c055b4a7905e
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-25 10:15:33 +0300

    add flat surface builder

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -12,200 +12,79 @@ import java.util.Scanner;
 class Player {
 
 	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt(); // the number of cards for player 1
-		Queue<Card> player1 = new LinkedList<>();
-		for (int i = 0; i < n; i++) {
-			player1.offer(getCard(in.next())); // the n cards of player 1
-		}
-		int m = in.nextInt(); // the number of cards for player 2
-		Queue<Card> player2 = new LinkedList<>();
-		for (int i = 0; i < m; i++) {
-			player2.offer(getCard(in.next())); // the m cards of player 2
-		}
+        Scanner in = new Scanner(System.in);
+        int surfaceN = in.nextInt(); // the number of points used to draw the surface of Mars.
+        int previousX = -1;
+        int previousY = -1;
+        int flatStart = -1;
+        int flatEnd = -1;
+        for (int i = 0; i < surfaceN; i++) {
+            int landX = in.nextInt(); // X coordinate of a surface point. (0 to 6999)
+            int landY = in.nextInt(); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+            if (landY == previousY)  {
+            	flatStart = previousX;
+            	flatEnd = landX;
+            }
+            previousX = landX;
+            previousY = landY;
+        }
 
-		// Write an action using System.out.println()
-		// To debug: System.err.println("Debug messages...");
+        // game loop
+        while (true) {
+            int X = in.nextInt();
+            int Y = in.nextInt();
+            int hSpeed = in.nextInt(); // the horizontal speed (in m/s), can be negative.
+            int vSpeed = in.nextInt(); // the vertical speed (in m/s), can be negative.
+            int fuel = in.nextInt(); // the quantity of remaining fuel in liters.
+            int rotate = in.nextInt(); // the rotation angle in degrees (-90 to 90).
+            int power = in.nextInt(); // the thrust power (0 to 4).
 
-		System.out.println(new Battle(player1, player2).getResult());
-	}
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+
+            // rotate power. rotate is the desired rotation angle. power is the desired thrust power.
+            System.out.println("-20 3");
+        }
+
+    }
 	
-	static class Battle {
-		private final Queue<Card> player1;
-		private final Queue<Card> player2;
-		private final Queue<Card> player1PreviousBattleCards;
-		private final Queue<Card> player2PreviousBattleCards;
-		private final Card player1BattleCard;
-		private final Card player2BattleCard;
-		private final int round;
-		
-		public Battle(Collection<Card> player1, Collection<Card> player2) {
-			this(player1, player2, new LinkedList<>(), new LinkedList<>(), 0);
-		}
-		
-		private Battle(Collection<Card> player1, Collection<Card> player2, Collection<Card> player1PreviousBattleCards,
-				Collection<Card> player2PreviousBattleCards, int previousRound) {
-			this.player1 = new LinkedList<>(player1);
-			this.player2 = new LinkedList<>(player2);
-			this.player1PreviousBattleCards = new LinkedList<>(player1PreviousBattleCards);
-			this.player2PreviousBattleCards = new LinkedList<>(player2PreviousBattleCards);
-			player1BattleCard = this.player1.poll();
-			this.player1PreviousBattleCards.offer(player1BattleCard);
-			player2BattleCard = this.player2.poll();
-			this.player2PreviousBattleCards.offer(player2BattleCard);
-			this.round = previousRound + 1;
-		}
-		
-		public Result getResult() {
-			if (player1BattleCard == null || player2BattleCard == null) {
-				return pat();
-			}
-			int cardsBattleScore = new CardComparator().compare(player1BattleCard, player2BattleCard);
-			if (cardsBattleScore == 0) {
-				if (moveWarCards(player1, player1PreviousBattleCards) &&
-					moveWarCards(player2, player2PreviousBattleCards)) {
-					return new Battle(player1, player2, player1PreviousBattleCards,
-							player2PreviousBattleCards, round - 1).getResult();
-				}
-				return pat();
-			}
-			if (cardsBattleScore < 0) {
-				if (player1.isEmpty()) {
-					return new Result("2 "+round);
-				}
-				player2.addAll(player1PreviousBattleCards);
-				player2.addAll(player2PreviousBattleCards);
-				
-			}
-			if (cardsBattleScore > 0) {
-				if (player2.isEmpty()) {
-					return new Result("1 "+round);
-				}
-				player1.addAll(player1PreviousBattleCards);
-				player1.addAll(player2PreviousBattleCards);
-			}
-			player1PreviousBattleCards.clear();
-			player2PreviousBattleCards.clear();
-			
-			return new Battle(player1, player2, player1PreviousBattleCards,
-					player2PreviousBattleCards, round).getResult();
-		}
-
-
-		private boolean moveWarCards(Queue<Card> player, Queue<Card> playerPreviousBattleCards) {
-			for (int i = 0; i < 3; i++) {
-				Card card = player.poll();
-				if (card == null) {
-					return false;
-				}
-				playerPreviousBattleCards.add(card);
-			}
-			return true;
-		}
-		
-		private static Result pat() {
-			return new Result("PAT");
-		}
-	}
-	
-	static class Result {
-		private final String result;
-
-		public Result(String result) {
-			this.result = result;
-		}
-
-		@Override
-		public String toString() {
-			return result;
-		}
-	}
-
-	static Card getCard(String description) {
-		int lastCharacterIndex = description.length() - 1;
-		Card.Suit suit = getEnumFromFirstChar(Card.Suit.class, description.charAt(lastCharacterIndex));
-		
-		try {
-			int rank = Integer.parseInt(description.substring(0, lastCharacterIndex));
-			return new LowCard(suit, rank);
-		}
-		catch (NumberFormatException nfe) {
-			return new HighCard(suit, getEnumFromFirstChar(Card.Honour.class, description.charAt(0)));
-		}
-	}
-	
-	static <T extends Enum<T>> T getEnumFromFirstChar(Class<T> clazz, char firstChar) {
-		return Arrays.stream(clazz.getEnumConstants()).filter(
-				enumConstant -> enumConstant.toString().charAt(0) == firstChar).findFirst().get();
-	}
-	
-	interface Card {
-		enum Suit {
-			SPADES, DIAMONDS, CLUBS, HEARTS;
-		}
-		
-		enum Honour {
-			JACK, QUEEN, KING, ACE;
-		}
-
-		Suit getSuit();
-	}
-
-	static class LowCard implements Card {
-
-		private final Suit suit;
-		private final int rank;
-
-		public LowCard(Suit suit, int rank) {
-			this.suit = suit;
-			this.rank = rank;
-		}
-
-		@Override
-		public Suit getSuit() {
-			return suit;
-		}
-
-		public int getRank() {
-			return rank;
-		}
-
-	}
-	
-	static class HighCard implements Card {
-		private final Suit suit;
-		private final Honour rank;
-		
-		public HighCard(Suit suit, Honour rank) {
-			this.suit = suit;
-			this.rank = rank;
-		}
-
-		@Override
-		public Suit getSuit() {
-			return suit;
-		}
-		
-		public Honour getRank() {
-			return rank;
-		}
-	}
-	
-	static class CardComparator implements Comparator<Card> {
-
-		@Override
-		public int compare(Card o1, Card o2) {
-			if (o1 instanceof LowCard) {
-				if (o2 instanceof LowCard) {
-					return ((LowCard) o1).getRank() - ((LowCard) o2).getRank();
-				}
-				return -1;
-			}
-			if (o2 instanceof LowCard) {
-				return 1;
-			}
-			return ((HighCard) o1).getRank().compareTo(((HighCard) o2).getRank());
-		}
-		
-	}
+    static class FlatSurface {
+    	private final int start;
+    	private final int end;
+    	
+    	private FlatSurface(Builder builder) {
+    		start = builder.start;
+    		end = builder.end;
+    	}
+    	
+    	int getStart() {
+    		return start;
+    	}
+    	
+    	int getEnd() {
+    		return end;
+    	}
+    	
+    	static class Builder {
+    		private int previousX = -1;
+    		private int previousY = -1;
+    		private int start;
+    		private int end;
+    		
+    		Builder usingPoint(int landX, int landY) {
+    			if (landY == previousY)  {
+    				start = previousX;
+    				end = landX;
+                }
+                previousX = landX;
+                previousY = landY;
+                return this;
+    		}
+    		
+    		FlatSurface build() {
+    			return new FlatSurface(this);
+    		}
+    	}
+    }	
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -7,85 +7,13 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class PlayerTest {
-	private final Player.CardComparator cc = new Player.CardComparator();
 	@Test
-	public void lowCardLessThanHighCard() throws Exception {
-		assertThat(cc.compare(Player.getCard("10H"), Player.getCard("AS"))).isLessThan(0);
-	}
-	
-	@Test
-	public void highCardGreaterThanLowCard() throws Exception {
-		assertThat(cc.compare(Player.getCard("KD"), Player.getCard("2C"))).isGreaterThan(0);
-	}
-	
-	@Test
-	public void lowCardLessThanLowCard() throws Exception {
-		assertThat(cc.compare(Player.getCard("9H"), Player.getCard("10C"))).isLessThan(0);
-	}
-	
-	@Test
-	public void lowCardGreaterThanLowCard() throws Exception {
-		assertThat(cc.compare(Player.getCard("9H"), Player.getCard("2D"))).isGreaterThan(0);
-	}
-	
-	@Test
-	public void highCardGreaterThanHighCard() throws Exception {
-		assertThat(cc.compare(Player.getCard("KD"), Player.getCard("JC"))).isGreaterThan(0);
-	}
-	
-	@Test
-	public void lowCardsEqual() throws Exception {
-		assertThat(cc.compare(Player.getCard("9H"), Player.getCard("9D"))).isEqualTo(0);
-	}
-	
-	@Test
-	public void highCardsEqual() throws Exception {
-		assertThat(cc.compare(Player.getCard("QS"), Player.getCard("QS"))).isEqualTo(0);
-	}
-	
-	@Test
-	public void player1WinsFastTest() throws Exception {
-		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D")), 
-							Arrays.asList(Player.getCard("9D"))).getResult().toString()).isEqualTo("1 1");
-	}
-	
-	@Test
-	public void fastPat() throws Exception {
-		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D")), 
-							Arrays.asList(Player.getCard("10C"))).getResult().toString()).isEqualTo("PAT");
-	}
-	
-	@Test
-	public void player2WinsFastTest() throws Exception {
-		assertThat(new Player.Battle(Arrays.asList(Player.getCard("2D")), 
-							Arrays.asList(Player.getCard("9D"))).getResult().toString()).isEqualTo("2 1");
-	}
-	
-	@Test
-	public void player1WinsTwoRoundsTest() throws Exception {
-		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D"), Player.getCard("KC")), 
-							Arrays.asList(Player.getCard("9D"), Player.getCard("2D"))).getResult().toString())
-				.isEqualTo("1 2");
-	}
-	
-	@Test
-	public void complexPat() throws Exception {
-		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D"), Player.getCard("9S"), Player.getCard("8D"), Player.getCard("KH"), Player.getCard("7D"), Player.getCard("5H"), Player.getCard("6S")), 
-							Arrays.asList(Player.getCard("10H"), Player.getCard("7H"), Player.getCard("5C"), Player.getCard("QC"), Player.getCard("2C"), Player.getCard("4H"), Player.getCard("6D")))
-				.getResult().toString()).isEqualTo("PAT");
-	}
-	
-	@Test
-	public void complexPlayer1Wins() throws Exception {
-		assertThat(new Player.Battle(Arrays.asList(Player.getCard("10D"), Player.getCard("9S"), Player.getCard("8D"), Player.getCard("KH"), Player.getCard("7D"), Player.getCard("5H"), Player.getCard("7S")), 
-							Arrays.asList(Player.getCard("10H"), Player.getCard("7H"), Player.getCard("5C"), Player.getCard("QC"), Player.getCard("2C"), Player.getCard("4H"), Player.getCard("6D")))
-				.getResult().toString()).isEqualTo("1 3");
-	}
-	
-	@Test
-	public void player1WinsThreeRoundsTest() throws Exception {
-		assertThat(new Player.Battle(Arrays.asList(Player.getCard("AD"), Player.getCard("KC"), Player.getCard("QC")), 
-							Arrays.asList(Player.getCard("KH"), Player.getCard("QS"), Player.getCard("JC"))).getResult().toString())
-				.isEqualTo("1 3");
+	public void buildsFlatSurface() throws Exception {
+		Player.FlatSurface.Builder builder = new Player.FlatSurface.Builder();
+		Player.FlatSurface surface = builder.usingPoint(0, 10).usingPoint(10, 5).usingPoint(12, 5).usingPoint(20, 7).build();
+		
+		assertThat(surface.getStart()).isEqualTo(10);
+		assertThat(surface.getEnd()).isEqualTo(12);
 	}
 }
+

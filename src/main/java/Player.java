fc14a78518b71c4e6f2894ccd31bb3d750e78595
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-25 17:27:29 +0300

    fixed network cabling

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -12,75 +13,37 @@ class Player {
 	
 	public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt(); // the number of adjacency relations
-        for (int i = 0; i < n; i++) {
-            int xi = in.nextInt(); // the ID of a person which is adjacent to yi
-            int yi = in.nextInt(); // the ID of a person which is adjacent to xi
-            Node.get(xi).addNeighbor(Node.get(yi));
-            Node.get(yi).addNeighbor(Node.get(xi));
+        int length = in.nextInt();
+        long[] ys = new long[length];
+        long minX = Long.MAX_VALUE;
+        long maxX = Long.MIN_VALUE;
+        for (int i = 0; i < length; i++) {
+            int x = in.nextInt();
+            if (x < minX) {
+            	minX = x;
+            }
+            if (x > maxX) {
+            	maxX = x;
+            }
+            
+            ys[i] = in.nextInt();
         }
-        Node.buildLeaves();
-        int steps = 0;
-        do {
-        	Set<Node> leaves = Node.getLeaves();
-        	if (leaves.size() < 2) {
-        		break;
-        	}
-        	leaves.stream().forEach(Node::kill);
-        	steps++;
-        }
-        while(true);
+        long cableLength = computeCableLength(ys, minX, maxX);
+
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
 
-
-        // The minimal amount of steps required to completely propagate the advertisement
-        System.out.println(steps);
+        System.out.println(cableLength);
     }
-	
-	static class Node {
-		private static final Map<Integer, Node> NODES = new HashMap<>();
-		private static final Set<Node> LEAVES = new HashSet<>();
-		private final Set<Node> neighbors = new HashSet<>();
-		
-		private Node() {
-		}
-		
-		public static Set<Node> getLeaves() {
-			return new HashSet<>(LEAVES);
-		}
 
-		public void addNeighbor(Node node) {
-			neighbors.add(node);
-		}
-		
-		public void kill() {
-			neighbors.forEach(neighbor -> {
-				neighbor.neighbors.remove(this);
-				if (neighbor.isLeaf()) {
-					LEAVES.add(neighbor);
-				}
-			});
-			LEAVES.remove(this);
-		}
-		
-		public boolean isLeaf() {
-			return neighbors.size() == 1;
-		}
-		
-		public static Node get(Integer i) {
-			Node node = NODES.get(i);
-			if (node == null) {
-				node = new Node();
-				NODES.put(i, node);
-			}
-			return node;
-		}
-		
-		public static void buildLeaves() {
-			NODES.values().stream().filter(Node::isLeaf).forEach(LEAVES::add);
-		}
-
+	static long computeCableLength(long[] ys, long minX, long maxX) {
+		Arrays.sort(ys);
+		long median = ys[ys.length / 2];  
+        long cableLength = maxX - minX;
+        for (long y : ys) {
+        	cableLength += Math.abs(y - median);
+        }
+		return cableLength;
 	}
 	
 }

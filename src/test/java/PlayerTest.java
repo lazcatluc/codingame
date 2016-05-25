575d0a3dde7e212e5d2da89711bc85733412eb9c
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-25 13:19:57 +0300

    initial commit

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -7,14 +7,6 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class PlayerTest {
-	@Test
-	public void buildsFlatSurface() throws Exception {
-		Player.FlatSurface.Builder builder = new Player.FlatSurface.Builder();
-		Player.FlatSurface surface = builder.usingPoint(0, 10).usingPoint(10, 5).usingPoint(12, 5).usingPoint(20, 7).build();
-		
-		assertThat(surface.getDistanceFrom(15)).isEqualTo(-3);
-		assertThat(surface.getDistanceFrom(9)).isEqualTo(1);
-		assertThat(surface.getDistanceFrom(10)).isEqualTo(0);
-	}
+	
 }
 

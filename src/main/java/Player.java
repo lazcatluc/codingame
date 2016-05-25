5328ef0f96d4a65d7e0a55d461ccaf0686e2ba24
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-25 17:01:27 +0300

    add efficient centrality

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.function.IntUnaryOperator;
+import java.util.Set;
 
 /**
  * Auto-generated code below aims at helping you parse the standard input
@@ -11,137 +10,77 @@ import java.util.function.IntUnaryOperator;
  **/
 class Player {
 	
-	private static final Map<Integer, Room> ROOMS_BY_TYPE = new HashMap<>();
-	static {
-		ROOMS_BY_TYPE.put(1, new Room(new Path.Builder().build(),
-				  new Path.Builder().from(Direction.LEFT).build(),
-				  new Path.Builder().from(Direction.RIGHT).build()));
-		ROOMS_BY_TYPE.put(2, new Room(
-				  new Path.Builder().from(Direction.LEFT).to(Direction.RIGHT).build(),
-				  new Path.Builder().from(Direction.RIGHT).to(Direction.LEFT).build()));
-		ROOMS_BY_TYPE.put(3, new Room(new Path.Builder().build()));
-		ROOMS_BY_TYPE.put(4, new Room(
-				  new Path.Builder().from(Direction.RIGHT).build(),
-				  new Path.Builder().to(Direction.LEFT).build()));
-		ROOMS_BY_TYPE.put(5, new Room(
-				  new Path.Builder().from(Direction.LEFT).build(),
-				  new Path.Builder().to(Direction.RIGHT).build()));
-		ROOMS_BY_TYPE.put(6, new Room(
-				  new Path.Builder().from(Direction.LEFT).to(Direction.RIGHT).build(),
-				  new Path.Builder().from(Direction.RIGHT).to(Direction.LEFT).build()));
-		ROOMS_BY_TYPE.put(7, new Room(new Path.Builder().build(),
-				  new Path.Builder().from(Direction.RIGHT).build()));
-		ROOMS_BY_TYPE.put(8, new Room(
-				  new Path.Builder().from(Direction.LEFT).build(),
-				  new Path.Builder().from(Direction.RIGHT).build()));
-		ROOMS_BY_TYPE.put(9, new Room(new Path.Builder().build(),
-				  new Path.Builder().from(Direction.LEFT).build()));
-		ROOMS_BY_TYPE.put(10, new Room(
-				  new Path.Builder().to(Direction.LEFT).build()));
-		ROOMS_BY_TYPE.put(11, new Room(
-				  new Path.Builder().to(Direction.RIGHT).build()));
-		ROOMS_BY_TYPE.put(12, new Room(
-				  new Path.Builder().from(Direction.RIGHT).build()));
-		ROOMS_BY_TYPE.put(13, new Room(
-				  new Path.Builder().from(Direction.LEFT).build()));
-	}
-	
 	public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int width = in.nextInt(); // number of columns.
-        int height = in.nextInt(); // number of rows.
-        int[][] roomTypes = new int[width][height];
-        in.nextLine();
-        for (int i = 0; i < height; i++) {
-            String[] line = in.nextLine().split(" "); // represents a line in the grid and contains W integers. Each integer represents one room of a given type.
-            for (int j = 0; j < width; j++) {
-            	roomTypes[j][i] = Integer.parseInt(line[j]);
-            }
+        int n = in.nextInt(); // the number of adjacency relations
+        for (int i = 0; i < n; i++) {
+            int xi = in.nextInt(); // the ID of a person which is adjacent to yi
+            int yi = in.nextInt(); // the ID of a person which is adjacent to xi
+            Node.get(xi).addNeighbor(Node.get(yi));
+            Node.get(yi).addNeighbor(Node.get(xi));
         }
-        int exitX = in.nextInt(); // the coordinate along the X axis of the exit (not useful for this first mission, but must be read).
-
-        // game loop
-        while (true) {
-            int xi = in.nextInt();
-            int yi = in.nextInt();
-            Direction direction = Direction.valueOf(in.next());
-            String result = ROOMS_BY_TYPE.get(roomTypes[xi][yi]).getOut(direction).transform(xi, yi);
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
-
-
-            // One line containing the X Y coordinates of the room in which you believe Indy will be on the next turn.
-            System.out.println(result);
+        Node.buildLeaves();
+        int steps = 0;
+        do {
+        	Set<Node> leaves = Node.getLeaves();
+        	if (leaves.size() < 2) {
+        		break;
+        	}
+        	leaves.stream().forEach(Node::kill);
+        	steps++;
         }
+        while(true);
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+
+        // The minimal amount of steps required to completely propagate the advertisement
+        System.out.println(steps);
     }
 	
-	enum Direction {
-		TOP(x -> x, y -> y - 1), 
-		RIGHT(x -> x + 1, y -> y), 
-		BOTTOM(x -> x, y -> y + 1), 
-		LEFT(x -> x - 1, y -> y);
+	static class Node {
+		private static final Map<Integer, Node> NODES = new HashMap<>();
+		private static final Set<Node> LEAVES = new HashSet<>();
+		private final Set<Node> neighbors = new HashSet<>();
 		
-		private final IntUnaryOperator transformX;
-		private final IntUnaryOperator transformY;
-		
-		private Direction(IntUnaryOperator transformX, IntUnaryOperator transformY) {
-			this.transformX = transformX;
-			this.transformY = transformY;
+		private Node() {
 		}
 		
-		public String transform(int x, int y) {
-			return transformX.applyAsInt(x) + " " + transformY.applyAsInt(y);
-		}
-	}
-	
-	static class Path {
-		private final Direction in;
-		private final Direction out;
-		
-		public Path(Builder builder) {
-			this.in = builder.in;
-			this.out = builder.out;
-		}
-		
-		static class Builder {
-			private Direction in = Direction.TOP;
-			private Direction out = Direction.BOTTOM;
-			
-			public Builder from(Direction in) {
-				this.in = in;
-				return this;
-			}
-			
-			public Builder to(Direction out) {
-				this.out = out;
-				return this;
-			}
-			
-			public Path build() {
-				return new Path(this);
-			}
+		public static Set<Node> getLeaves() {
+			return new HashSet<>(LEAVES);
 		}
 
-		public boolean startsWith(Direction direction) {
-			return in.equals(direction);
+		public void addNeighbor(Node node) {
+			neighbors.add(node);
 		}
 		
-		public Direction getOut() {
-			return out;
+		public void kill() {
+			neighbors.forEach(neighbor -> {
+				neighbor.neighbors.remove(this);
+				if (neighbor.isLeaf()) {
+					LEAVES.add(neighbor);
+				}
+			});
+			LEAVES.remove(this);
 		}
+		
+		public boolean isLeaf() {
+			return neighbors.size() == 1;
+		}
+		
+		public static Node get(Integer i) {
+			Node node = NODES.get(i);
+			if (node == null) {
+				node = new Node();
+				NODES.put(i, node);
+			}
+			return node;
+		}
+		
+		public static void buildLeaves() {
+			NODES.values().stream().filter(Node::isLeaf).forEach(LEAVES::add);
+		}
+
 	}
-	
-	static class Room {
-		private final Collection<Path> paths;
-		
-		public Room(Path...paths) {
-			this.paths = Arrays.asList(paths);
-		}
-		
-		public Direction getOut(Direction in) {
-			return paths.stream().filter(path -> path.startsWith(in)).map(Path::getOut).findFirst().get();
-		}
-	}
-	
 	
 }

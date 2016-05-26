5ea3f2ceaf23125421e08d4c8139eebd149be009
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-26 09:55:29 +0300

    initial commit

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -13,44 +13,17 @@ class Player {
 	
 	public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int firstNumber = in.nextInt();
-        int line = in.nextInt();
-        List<Integer> currentLine = Arrays.asList(firstNumber);
-        
-        while (line > 1) {
-        	line--;
-        	currentLine = transform(currentLine);
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            String telephone = in.next();
         }
 
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
 
-        System.out.println(print(currentLine));
+
+        // The number of elements (referencing a number) stored in the structure.
+        System.out.println("number");
     }
-
-	static List<Integer> transform(List<Integer> currentLine) {
-		if (currentLine.isEmpty()) {
-			return Collections.emptyList();
-		}
-		List<Integer> result = new ArrayList<>();
-		List<Integer> numbersToAdd = new ArrayList<>(currentLine);
-		Integer toAdd = 1;
-		while (numbersToAdd.size() > 1 && numbersToAdd.get(0).equals(numbersToAdd.get(1))) {
-			toAdd++;
-			numbersToAdd.remove(0);
-		}
-		Integer currentlyRemoved = numbersToAdd.remove(0);
-		result.add(toAdd);
-		result.add(currentlyRemoved);
-		result.addAll(transform(numbersToAdd));
-		return result;
-	}
-
-	public static String print(List<Integer> asList) {
-		StringBuilder sb = new StringBuilder();
-		asList.forEach(i -> sb.append(i).append(" "));
-		sb.deleteCharAt(sb.length() - 1);
-		return sb.toString();
-	}
 	
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -7,29 +7,6 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class PlayerTest {
-	@Test
-	public void transformsSingleNumberInto1AndNumber() throws Exception {
-		assertThat(Player.transform(Arrays.asList(5))).isEqualTo(Arrays.asList(1, 5));
-	}
 	
-	@Test
-	public void transformsSingleNumberTwoTimesInto2AndNumber() throws Exception {
-		assertThat(Player.transform(Arrays.asList(1, 1))).isEqualTo(Arrays.asList(2, 1));
-	}
-	
-	@Test
-	public void transformsTwoNumbersTwoTimesIntoEach1AndNumber() throws Exception {
-		assertThat(Player.transform(Arrays.asList(2, 1))).isEqualTo(Arrays.asList(1, 2, 1, 1));
-	}
-	
-	@Test
-	public void transformsNumberThreeTimesInto3AndNumber() throws Exception {
-		assertThat(Player.transform(Arrays.asList(1, 1, 1))).isEqualTo(Arrays.asList(3, 1));
-	}
-	
-	@Test
-	public void printsList() throws Exception {
-		assertThat(Player.print(Arrays.asList(1, 2, 1, 1))).isEqualTo("1 2 1 1");
-	}
 }
 

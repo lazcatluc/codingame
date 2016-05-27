93caa0e1f25e293048f3eb58271480faa4ba29c6
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-27 15:59:18 +0300

    add digit reading

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,6 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -10,13 +10,14 @@ import java.util.Scanner;
  **/
 class Player {
 	
+	static final int TOTAL_DIGITS = 20;
+	static final Map<List<String>, Integer> DIGITS = new HashMap<>(TOTAL_DIGITS);
+	
 	public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int L = in.nextInt();
-        int H = in.nextInt();
-        for (int i = 0; i < H; i++) {
-            String numeral = in.next();
-        }
+        int digitLength = in.nextInt();
+        int digitHeight = in.nextInt();
+        buildDigits(in, digitLength, digitHeight);
         int S1 = in.nextInt();
         for (int i = 0; i < S1; i++) {
             String num1Line = in.next();
@@ -32,5 +33,19 @@ class Player {
 
         System.out.println("result");
     }
+
+	static List<List<String>> buildDigits(Scanner in, int digitLength, int digitHeight) {
+		List<List<String>> digits = new ArrayList<>(TOTAL_DIGITS);
+        for (int i = 0; i < TOTAL_DIGITS; i++) {
+        	digits.add(new ArrayList<>(digitHeight));
+        }
+        for (int i = 0; i < digitHeight; i++) {
+            String numeral = in.next();
+            for (int j = 0; j < TOTAL_DIGITS; j++) {
+            	digits.get(j).add(numeral.substring(j*digitLength, (j + 1)*digitLength));
+            }
+        }
+        return digits;
+	}
 }
 
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -2,12 +2,23 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Scanner;
 
 import org.junit.Test;
 
 public class PlayerTest {
-	
+	@Test
+	public void buildsDigits() throws Exception {
+		String digitInput=".oo.o...oo..ooo.oooo....o...oo..ooo.oooo____o...oo..ooo.oooo____o...oo..ooo.oooo\n"+
+						  "o..o................____________________________________________________________\n"+
+						  ".oo.........................................____________________________________\n"+
+					  	  "................................................................________________\n";
+		List<List<String>> digits = Player.buildDigits(new Scanner(new ByteArrayInputStream(digitInput.getBytes())),4,4);
+		
+		assertThat(digits.get(5)).isEqualTo(Arrays.asList("....","____","....","...."));
+	}
 }

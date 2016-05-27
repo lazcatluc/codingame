e3cf94dc2bdd938b2fab31d138b7282e9dd610fe
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-27 15:10:13 +0300

    complete the gift

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -12,16 +12,33 @@ class Player {
 	
 	public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int C = in.nextInt();
-        for (int i = 0; i < N; i++) {
-            int B = in.nextInt();
+        int giftBearers = in.nextInt();
+        int giftPrice = in.nextInt();
+        int[] budgets = new int[giftBearers];
+        for (int i = 0; i < giftBearers; i++) {
+            budgets[i] = in.nextInt();
         }
-
+        int totalBudget = Arrays.stream(budgets).sum();
+        if (totalBudget < giftPrice) {
+        	System.out.println("IMPOSSIBLE");
+        	return;
+        }
+        Arrays.sort(budgets);
+        
+        for (int i = 0; i < giftBearers; i++) {
+        	int contributors = giftBearers - i;
+        	int fairBudget = giftPrice / contributors;
+			if (budgets[i] > fairBudget) {
+        		System.out.println(fairBudget);
+        		giftPrice -= fairBudget;
+        		continue;
+        	}
+			System.out.println(budgets[i]);
+			giftPrice -= budgets[i];
+        }
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
 
-        System.out.println("IMPOSSIBLE");
     }
 }
 

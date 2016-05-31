0cfb6cff21d5e5e9a8c4d136fb9f84a1d543bb04
Lazar <LCLazar@DBDC.luxoft.com>
2016-05-31 11:45:50 +0300

    killed skynet with long gateways

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -17,27 +20,43 @@ class Player {
 	
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int numberOfNodes = in.nextInt(); 
-        int numberOfLinks = in.nextInt(); 
-        int numberOfGateways = in.nextInt(); 
-        for (int i = 0; i < numberOfLinks; i++) {
-            int firstNode = in.nextInt(); // N1 and N2 defines a link between these nodes
-            int secondNode = in.nextInt();
-            Node.get(firstNode).addLink(Node.get(secondNode));
-        }
-        for (int i = 0; i < numberOfGateways; i++) {
-            markGateway(in.nextInt()); // the index of a gateway node
-        }
+        readData(in);
 
         // game loop
         while (true) {
-            int skynetAgentIndex = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
+            int skynetAgentIndex = nextInt(in); // The index of the node on which the Skynet agent is positioned this turn
             
             List<Node> shortestPath = findShortestPathToGatewaysFrom(skynetAgentIndex);
-            
-            System.out.println(printLink(shortestPath.get(0), shortestPath.get(1)));
+            Node nodeWithGateways = shortestPath.get(0);
+			for (Node node : GATEWAYS) {
+				if (nodeWithGateways.getLinks().contains(node)) {
+					serverLinkBetween(nodeWithGateways, node);
+					System.out.println(printLink(nodeWithGateways, node));
+					break;
+				}
+			}
         }
     }
+
+	protected static void readData(Scanner in) {
+		int numberOfNodes = nextInt(in); 
+		int numberOfLinks = nextInt(in); 
+        int numberOfGateways = nextInt(in); 
+        for (int i = 0; i < numberOfLinks; i++) {
+            int firstNode = nextInt(in); // N1 and N2 defines a link between these nodes
+            int secondNode = nextInt(in);
+            Node.get(firstNode).addLink(Node.get(secondNode));
+        }
+        for (int i = 0; i < numberOfGateways; i++) {
+            markGateway(nextInt(in)); // the index of a gateway node
+        }
+	}
+
+	protected static int nextInt(Scanner in) {
+		int x = in.nextInt();
+		System.err.println(x);
+		return x;
+	}
     
     static class Node {
     	private final int index;
@@ -51,6 +70,16 @@ class Player {
     	public void addLink(Node node) {
     		linked.add(node);
     		node.linked.add(this);
+    	}
+    	
+    	public boolean isConnectedToGateway() {
+    		return getGatewaysConnected() > 0;
+    	}
+    	
+    	public int getGatewaysConnected() {
+    		Set<Node> gateways = new HashSet<>(GATEWAYS);
+    		gateways.retainAll(linked);
+    		return gateways.size();
     	}
     	
     	public static Node get(int index) {
@@ -77,11 +106,19 @@ class Player {
 			return String.valueOf(index);
 		}
 		
+		public static void clearNodes() {
+			NODES.clear();
+		}
+	
     }
 
 	public static void severLinkBetween(int i, int j) {
-		Node.get(i).severLink(Node.get(j));
-		Node.get(j).severLink(Node.get(i));
+		serverLinkBetween(Node.get(i),Node.get(j));
+	}
+	
+	public static void serverLinkBetween(Node first, Node second) {
+		second.severLink(first);
+		first.severLink(second);
 	}
 
 	public static void markGateway(int index) {
@@ -89,24 +126,56 @@ class Player {
 	}
 
 	public static Set<List<Node>> findPathsToGatewaysFrom(int startingPoint) {
-		return findPathsToGatewaysFrom(Node.get(startingPoint), Collections.emptyList());
-		
+		int level = 1;
+		do {
+			Set<List<Node>> pathsForLevel = findPathsToGatewaysFrom(Node.get(startingPoint), Collections.emptyList(), level);
+			if (!pathsForLevel.isEmpty()) {
+				return pathsForLevel;
+			}
+			level++;
+		}
+		while(true);
 	}
 	
-	private static Set<List<Node>> findPathsToGatewaysFrom(Node start, List<Node> previousNodes) {
+	protected static Set<List<Node>> findPathsToGatewaysFrom(Node start, List<Node> previousNodes, int lookUpToLevel) {
 		List<Node> completeNodes = new ArrayList<>(previousNodes);
 		completeNodes.add(start);
 		if (GATEWAYS.contains(start)) {
 			return Collections.singleton(completeNodes);
 		}
+		if (lookUpToLevel == 0) {
+			return Collections.emptySet();
+		}
 		Set<List<Node>> ret = new LinkedHashSet<>();
 		start.getLinks().stream().filter(node -> !previousNodes.contains(node))
-				.map(node -> findPathsToGatewaysFrom(node, completeNodes)).forEach(ret::addAll);
+				.map(node -> findPathsToGatewaysFrom(node, completeNodes, lookUpToLevel - 1)).forEach(ret::addAll);
 		return ret;
 	}
 
 	public static List<Node> findShortestPathToGatewaysFrom(int startingPoint) {
-		return findPathsToGatewaysFrom(startingPoint).stream().min((path1, path2) -> path1.size() - path2.size()).get();
+		Set<List<Node>> pathsToGateways = findPathsToGatewaysFrom(startingPoint);
+		return findNodesWithMostGateways(pathsToGateways);
+	}
+
+	protected static List<Node> findNodesWithMostGateways(Set<List<Node>> minimalPaths) {
+		Set<Node> closeNodes = minimalPaths.stream().map(Player::findNodeBeforeGateway).collect(Collectors.toSet());
+		List<Node> nodesWithDirectConnections = new ArrayList<>(findOtherNeighborsWithDirectConnections(closeNodes));
+		Collections.sort(nodesWithDirectConnections, (node1, node2) -> node2.getGatewaysConnected() - node1.getGatewaysConnected());
+		return nodesWithDirectConnections;
+	}
+	
+	protected static Set<Node> findOtherNeighborsWithDirectConnections(Set<Node> startingNodes) {
+		Set<Node> newNodes = new LinkedHashSet<>(startingNodes);
+		startingNodes.stream().map(Node::getLinks)
+			.forEach(set -> set.stream().filter(Node::isConnectedToGateway).forEach(newNodes::add));
+		if (newNodes.equals(startingNodes)) {
+			return startingNodes;
+		}
+		return findOtherNeighborsWithDirectConnections(newNodes);
+	}
+	
+	private static Node findNodeBeforeGateway(List<Node> path) {
+		return path.get(path.size() - 2);
 	}
 
 	public static String printLink(Node start, Node finish) {
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,13 +1,25 @@
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 
+import org.junit.After;
 import org.junit.Test;
 
 public class PlayerTest {
+	
+	@After
+	public void tearDown() {
+		Player.GATEWAYS.clear();
+		Player.Node.clearNodes();
+	}
+	
 	@Test
 	public void addingALinkAddsTheOppositeLink() throws Exception {
 		Player.Node.get(0).addLink(Player.Node.get(1));
@@ -37,7 +49,6 @@ public class PlayerTest {
 				Player.Node.get(0), Player.Node.get(1), Player.Node.get(2)),Arrays.asList(
 						Player.Node.get(0), Player.Node.get(1), Player.Node.get(3)))));
 		
-		Player.GATEWAYS.clear();
 	}
 	
 	@Test
@@ -50,8 +61,8 @@ public class PlayerTest {
 		Player.markGateway(2);
 		Player.markGateway(4);
 		
-		assertThat(Player.findShortestPathToGatewaysFrom(0)).isEqualTo(Arrays.asList(
-				Player.Node.get(0), Player.Node.get(1), Player.Node.get(2)));
+		assertThat(Player.findNodesWithMostGateways(Player.findPathsToGatewaysFrom(0)).get(0))
+			.isEqualTo(Player.Node.get(1));
 	}
 	
 	@Test
@@ -59,5 +70,187 @@ public class PlayerTest {
 		Player.Node.get(0).addLink(Player.Node.get(1));
 		
 		assertThat(Player.printLink(Player.Node.get(0), Player.Node.get(1))).isEqualTo("0 1");
+	}
+	
+	@Test
+	public void longNetwork() throws Exception {
+		String input = "37\n"+
+				"81\n"+
+				"4\n"+
+				"2\n"+
+				"5\n"+
+				"14\n"+
+				"13\n"+
+				"16\n"+
+				"13\n"+
+				"19\n"+
+				"21\n"+
+				"13\n"+
+				"7\n"+
+				"16\n"+
+				"8\n"+
+				"35\n"+
+				"5\n"+
+				"2\n"+
+				"35\n"+
+				"10\n"+
+				"0\n"+
+				"8\n"+
+				"3\n"+
+				"23\n"+
+				"16\n"+
+				"0\n"+
+				"1\n"+
+				"31\n"+
+				"17\n"+
+				"19\n"+
+				"22\n"+
+				"12\n"+
+				"11\n"+
+				"1\n"+
+				"2\n"+
+				"1\n"+
+				"4\n"+
+				"14\n"+
+				"9\n"+
+				"17\n"+
+				"16\n"+
+				"30\n"+
+				"29\n"+
+				"32\n"+
+				"22\n"+
+				"28\n"+
+				"26\n"+
+				"24\n"+
+				"23\n"+
+				"20\n"+
+				"19\n"+
+				"15\n"+
+				"13\n"+
+				"18\n"+
+				"17\n"+
+				"6\n"+
+				"1\n"+
+				"29\n"+
+				"28\n"+
+				"15\n"+
+				"14\n"+
+				"9\n"+
+				"13\n"+
+				"32\n"+
+				"18\n"+
+				"25\n"+
+				"26\n"+
+				"1\n"+
+				"7\n"+
+				"34\n"+
+				"35\n"+
+				"33\n"+
+				"34\n"+
+				"27\n"+
+				"16\n"+
+				"27\n"+
+				"26\n"+
+				"23\n"+
+				"25\n"+
+				"33\n"+
+				"3\n"+
+				"16\n"+
+				"30\n"+
+				"25\n"+
+				"24\n"+
+				"3\n"+
+				"2\n"+
+				"5\n"+
+				"4\n"+
+				"31\n"+
+				"32\n"+
+				"27\n"+
+				"25\n"+
+				"19\n"+
+				"3\n"+
+				"17\n"+
+				"8\n"+
+				"4\n"+
+				"2\n"+
+				"32\n"+
+				"17\n"+
+				"10\n"+
+				"11\n"+
+				"29\n"+
+				"27\n"+
+				"30\n"+
+				"27\n"+
+				"6\n"+
+				"4\n"+
+				"24\n"+
+				"15\n"+
+				"9\n"+
+				"10\n"+
+				"34\n"+
+				"2\n"+
+				"9\n"+
+				"7\n"+
+				"11\n"+
+				"6\n"+
+				"33\n"+
+				"2\n"+
+				"14\n"+
+				"10\n"+
+				"12\n"+
+				"6\n"+
+				"0\n"+
+				"6\n"+
+				"19\n"+
+				"17\n"+
+				"20\n"+
+				"3\n"+
+				"21\n"+
+				"20\n"+
+				"21\n"+
+				"32\n"+
+				"15\n"+
+				"16\n"+
+				"0\n"+
+				"9\n"+
+				"23\n"+
+				"27\n"+
+				"11\n"+
+				"0\n"+
+				"28\n"+
+				"27\n"+
+				"22\n"+
+				"18\n"+
+				"3\n"+
+				"1\n"+
+				"23\n"+
+				"15\n"+
+				"18\n"+
+				"19\n"+
+				"7\n"+
+				"0\n"+
+				"19\n"+
+				"8\n"+
+				"21\n"+
+				"22\n"+
+				"7\n"+
+				"36\n"+
+				"13\n"+
+				"36\n"+
+				"8\n"+
+				"36\n"+
+				"0\n"+
+				"16\n"+
+				"18\n"+
+				"26\n"+
+				"2\n";
+		Player.readData(new Scanner(new ByteArrayInputStream(input.getBytes())));
+		Set<List<Player.Node>> paths = Player.findPathsToGatewaysFrom(2);	
+		System.out.println(paths);
+		assertThat(paths.size()).isEqualTo(1);
+		assertThat(Player.findOtherNeighborsWithDirectConnections(Collections.singleton(Player.Node.get(1))).size())
+			.isEqualTo(13);
+		
+		System.out.println(Player.findShortestPathToGatewaysFrom(2));
 	}
 }

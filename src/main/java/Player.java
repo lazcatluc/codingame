ec753db95e8e633b8901e8dabc2bb6f56b4d6f0b
Lazar <LCLazar@DBDC.luxoft.com>
2016-06-06 15:19:33 +0300

    add backtracking

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Comparator;
+import java.util.EnumSet;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -20,24 +22,192 @@ class Player {
 		Scanner in = new Scanner(System.in);
 		int motorbikes = in.nextInt(); // the amount of motorbikes to control
 		int necessaryToSurvive = in.nextInt(); // the minimum amount of motorbikes that must
-		
+		Runway runway = new Runway(in);
+		Queue<Action> actions = new LinkedList<>();
 		// game loop
 		while (true) {
-			int S = in.nextInt(); // the motorbikes' speed
+			Set<Motorcycle> motorcycles = new HashSet<>();
+			int speed = in.nextInt(); // the motorbikes' speed
 			for (int i = 0; i < motorbikes; i++) {
-				int X = in.nextInt(); // x coordinate of the motorbike
-				int Y = in.nextInt(); // y coordinate of the motorbike
-				int A = in.nextInt(); // indicates whether the motorbike is
+				int x = in.nextInt(); // x coordinate of the motorbike
+				int y = in.nextInt(); // y coordinate of the motorbike
+				int active = in.nextInt(); // indicates whether the motorbike is
 										// activated "1" or detroyed "0"
+				motorcycles.add(new Motorcycle.Builder().atX(x).atY(y)
+						.goingAt(speed).active(active).build());
 			}
-
+			if (actions.isEmpty()) {
+				SearchMotorcyclesPath searchMotorcyclesPath = new SearchMotorcyclesPath(motorcycles, runway, necessaryToSurvive);
+				actions.addAll(searchMotorcyclesPath.getActionsToGoal());
+			}
 			// Write an action using System.out.println()
 			// To debug: System.err.println("Debug messages...");
 
 			// A single line containing one of 6 keywords: SPEED, SLOW, JUMP,
 			// WAIT, UP, DOWN.
-			System.out.println("SPEED");
+			if (actions.isEmpty()) {
+				System.out.println(Action.WAIT);
+			}
+			else {
+				System.out.println(actions.poll());
+			}
 		}
+	}
+	
+	static class SearchMotorcyclesPath {
+		private final int maxPathLength = 50;
+		private final Set<Motorcycle> motorcycles;
+		private final Runway runway;
+		private final int goal;
+		private final int necessaryToSurvive;
+		private final int okSpeed;
+		
+		public SearchMotorcyclesPath(Collection<Motorcycle> motorcycles, Runway runway, int necessaryToSurvive) {
+			this.motorcycles = new HashSet<>(motorcycles);
+			this.runway = runway;
+			goal = runway.length();
+			okSpeed = maxPathLength / goal + 1;
+			this.necessaryToSurvive = necessaryToSurvive; 
+		}
+		
+		private boolean isValid(MotorcyclesOnRunway motorcyclesOnRunway) {
+			return motorcyclesOnRunway.activeMotorcycles() >= necessaryToSurvive;
+		}
+		
+		private boolean isGoal(MotorcyclesOnRunway motorcyclesOnRunway) {
+			return motorcyclesOnRunway.furthestMotorcycleLevel() >= goal;
+		}
+		
+		public List<Action> getActionsToGoal() {
+			return getActionsToGoal(new MotorcyclesOnRunway(motorcycles, runway, okSpeed), 0);
+		}
+
+		private List<Action> getActionsToGoal(MotorcyclesOnRunway motorcyclesOnRunway, int pathLength) {
+			if (pathLength > maxPathLength) {
+				return null;
+			}
+			if (!isValid(motorcyclesOnRunway)) {
+				return null;
+			}
+			if (isGoal(motorcyclesOnRunway)) {
+				return Collections.emptyList();
+			}
+			for (MotorcyclesOnRunway nextMotorcyclesOnRunway : motorcyclesOnRunway.nextMotorcyclesOnRunways()) {
+				List<Action> actions = getActionsToGoal(nextMotorcyclesOnRunway, pathLength + 1);
+				if (actions == null) {
+					continue;
+				}
+				Action action = getActionFor(nextMotorcyclesOnRunway, motorcyclesOnRunway);
+				List<Action> ret = new ArrayList<>(actions.size() + 1);
+				ret.add(action);
+				ret.addAll(actions);
+				return ret;
+			}
+			return null;
+		}
+
+		private Action getActionFor(MotorcyclesOnRunway nextMotorcyclesOnRunway,
+				MotorcyclesOnRunway currentMotorcyclesOnRunway) {
+			return currentMotorcyclesOnRunway.allowedActions().stream().filter(action -> 
+				new MotorcyclesOnRunway(currentMotorcyclesOnRunway.actedOnMotorcycles(action),
+						runway, okSpeed).equals(nextMotorcyclesOnRunway)).findAny().get();
+		}
+
+		
+	}
+	
+	static class MotorcyclesOnRunway {
+		private final Set<Motorcycle> motorcycles;
+		private final Runway runway;
+		private final int okSpeed;
+		
+		public MotorcyclesOnRunway(Collection<Motorcycle> motorcycles, Runway runway, int okSpeed) {
+			this.motorcycles = new HashSet<>(motorcycles);
+			this.runway = runway;
+			this.okSpeed = okSpeed;
+		}
+
+		public Set<Action> allowedActions() {
+			Set<Action> actions = EnumSet.allOf(Player.Action.class);
+			if (hasActiveMotorcycleOnLane(0)) {
+				actions.remove(Action.UP);
+			}
+			if (hasActiveMotorcycleOnLane(runway.lastLane())) {
+				actions.remove(Action.DOWN);
+			}
+			return actions;
+		}
+		
+		public List<MotorcyclesOnRunway> nextMotorcyclesOnRunways() {
+			List<MotorcyclesOnRunway> nextStates = allowedActions().stream().map(this::actedOnMotorcycles)
+					.map(afterActionMotorcycles -> new MotorcyclesOnRunway(afterActionMotorcycles, runway, okSpeed))
+					.collect(Collectors.toList());
+			Comparator<MotorcyclesOnRunway> c0 = (mor1, mor2) -> mor1.speedDelta() - mor2.speedDelta();
+			Comparator<MotorcyclesOnRunway> c1 = (mor1, mor2) -> mor1.activeMotorcycles() - mor2.activeMotorcycles();
+			Comparator<MotorcyclesOnRunway> c2 = (mor1, mor2) -> mor1.furthestMotorcycleLevel() - mor2.furthestMotorcycleLevel();
+			Collections.sort(nextStates, c2.reversed().thenComparing(c0).thenComparing(c1.reversed()));
+			return nextStates;
+		}
+		
+		private Set<Motorcycle> actedOnMotorcycles(Action action) {
+			return motorcycles.stream().map(motorcycle -> new MotorcyleActingOnRunway(motorcycle, runway, action))
+				.map(MotorcyleActingOnRunway::getAfterAction).collect(Collectors.toSet());
+		}
+		
+		public int activeMotorcycles() {
+			return (int)motorcycles.stream().filter(Motorcycle::isActive).count();
+		}
+		
+		public int furthestMotorcycleLevel() {
+			return motorcycles.stream().map(Motorcycle::getX).max((i1, i2) -> i1 - i2).orElse(0);
+		}
+		
+		public int speedDelta() {
+			return Math.abs(motorcycles.stream().findAny().get().getSpeed() - okSpeed);
+		}
+		
+		private boolean hasActiveMotorcycleOnLane(int lane) {
+			return motorcycles.stream().filter(Motorcycle::isActive).map(Motorcycle::getY).anyMatch(x -> x.equals(lane));
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((motorcycles == null) ? 0 : motorcycles.hashCode());
+			result = prime * result + ((runway == null) ? 0 : runway.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			MotorcyclesOnRunway other = (MotorcyclesOnRunway) obj;
+			if (motorcycles == null) {
+				if (other.motorcycles != null)
+					return false;
+			} else if (!motorcycles.equals(other.motorcycles))
+				return false;
+			if (runway == null) {
+				if (other.runway != null)
+					return false;
+			} else if (!runway.equals(other.runway))
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "MotorcyclesOnRunway [motorcycles=" + motorcycles + ", runway=" + runway + "]";
+		}
+		
+		
+
 	}
 	
 
@@ -52,31 +222,239 @@ class Player {
 			}
 		}
 		
+		public int length() {
+			return lanes[0].length();
+		}
+
+		public int lastLane() {
+			return lanes.length - 1;
+		}
+
 		public boolean isClear(int x, int y) {
+			if (x < 0) {
+				return false;
+			}
 			if (y < WIDTH && x < lanes[0].length()) {
 				return lanes[y].charAt(x) == '.';
 			}
-			return x >= lanes[0].length();
+			return x >= length();
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + Arrays.hashCode(lanes);
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Runway other = (Runway) obj;
+			if (!Arrays.equals(lanes, other.lanes))
+				return false;
+			return true;
 		}
 		
-		public boolean isPathClear(int fromX, int fromY, int toX, int toY) {
-			if (!isClear(fromX, fromY)) {
+		
+	}
+	
+	static class MotorcyleActingOnRunway {
+		private final Motorcycle motorcycle;
+		private final Runway runway;
+		private final Action action;
+		private final Motorcycle afterAction;
+		
+		public MotorcyleActingOnRunway(Motorcycle motorcycle, Runway runway, Action action) {
+			this.motorcycle = motorcycle;
+			this.runway = runway;
+			this.action = action;
+			this.afterAction = motorcycle.act(action);
+		}
+		
+		public Motorcycle getAfterAction() {
+			if (isPathClear()) {
+				return afterAction;
+			}
+			return new Motorcycle.Builder(motorcycle).active(false).build();
+		}
+		
+		public boolean isPathClear() {
+			if (!runway.isClear(motorcycle.getX(), motorcycle.getY())) {
 				return false;
 			}
-			if (!isClear(toX, toY)) {
+			if (!runway.isClear(afterAction.getX(), afterAction.getY())) {
 				return false;
 			}
-			for (int i = fromX + 1; i < toX; i++) {
-				int startY = fromY > toY ? toY : fromY;
-				int endY = fromY + toY - startY;
+			if (action == Action.JUMP) {
+				return true;
+			}
+			for (int i = motorcycle.getX() + 1; i < afterAction.getX(); i++) {
+				int startY = motorcycle.getY() > afterAction.getY() ? afterAction.getY() : motorcycle.getY();
+				int endY = motorcycle.getY() + afterAction.getY() - startY;
 				for (int j = startY; j <= endY; j++) {
-					if (!isClear(i, j)) {
+					if (!runway.isClear(i, j)) {
 						return false;
 					}
 				}
 			}
 			return true;
 		}
+
+		@Override
+		public String toString() {
+			return "MotorcyleActingOnRunway [motorcycle=" + motorcycle + ", action=" + action + ", afterAction="
+					+ afterAction + "]";
+		}
+		
+		
+	}
+	
+	enum Action {
+		SPEED, SLOW, JUMP, WAIT, UP, DOWN;
+	}
+	
+	static class Motorcycle {
+
+		private final int x;
+		private final int y;
+		private final boolean active;
+		private final int speed;
+		
+		private Motorcycle(Builder builder) {
+			this.x = builder.x;
+			this.y = builder.y;
+			this.active = builder.active;
+			this.speed = builder.speed;
+		}		
+		
+		public Motorcycle act(Action action) {
+			Builder builder = new Builder(this);
+			if (action == Action.SPEED) {
+				builder.goingAt(speed + 1);
+			}
+			if (action == Action.SLOW) {
+				builder.goingAt(speed - 1);
+			}
+			if (active) {
+				builder.atX(x + builder.speed);
+				if (action == Action.UP) {
+					builder.atY(y - 1);
+				}
+				if (action == Action.DOWN) {
+					builder.atY(y + 1);
+				}
+			}
+			return builder.build();
+		}
+		
+		static class Builder {
+			int x = 0;
+			int y = 0;
+			boolean active = true;
+			int speed = 0;
+			
+			public Builder() {
+				
+			}
+			
+			public Builder(Motorcycle motorcycle) {
+				x = motorcycle.x;
+				y = motorcycle.y;
+				active = motorcycle.active;
+				speed = motorcycle.speed;
+			}
+
+			public Builder atX(int x) {
+				this.x = x;
+				return this;
+			}
+			
+			public Builder atY(int y) {
+				this.y = y;
+				return this;
+			}
+			
+			public Builder goingAt(int speed) {
+				this.speed = speed;
+				return this;
+			}
+			
+			public Builder active(boolean active) {
+				this.active = active;
+				return this;
+			}
+			
+			public Builder active(int active) {
+				this.active = active > 0;
+				return this;
+			}
+			
+			public Motorcycle build() {
+				return new Motorcycle(this);
+			}
+		}
+
+		public int getSpeed() {
+			return speed;
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public int getY() {
+			return y;
+		}
+		
+		public boolean isActive() {
+			return active;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + (active ? 1231 : 1237);
+			result = prime * result + speed;
+			result = prime * result + x;
+			result = prime * result + y;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Motorcycle other = (Motorcycle) obj;
+			if (active != other.active)
+				return false;
+			if (speed != other.speed)
+				return false;
+			if (x != other.x)
+				return false;
+			if (y != other.y)
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "Motorcycle [x=" + x + ", y=" + y + ", active=" + active + ", speed=" + speed + "]";
+		}
+		
+		
 	}
 
 }
+
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -5,12 +5,8 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
-import org.junit.After;
 import org.junit.Test;
 
 public class PlayerTest {
@@ -19,32 +15,139 @@ public class PlayerTest {
 			"...00.....\n"+
 			"...0..0...\n"+
 			"...0......\n";
+	
+	private String heavyIn = 
+			"................000000000........00000........000.............00.\n"+
+			".0.0..................000....000......0.0..................00000.\n"+
+			"....000.........0.0...000................000............000000.0.\n"+
+			"............0.000000...........0000...............0.0.....000000.\n";
+
 	private Player.Runway newRunway() {
+		return newRunway(in);
+	}
+	
+	private Player.Runway newRunway(String in) {
 		return new Player.Runway(new Scanner(new ByteArrayInputStream(in.getBytes())));
 	}
 	
+	private Player.MotorcyleActingOnRunway newMotorcycleActingOnRunway(Player.Motorcycle motorcycle, Player.Action action) {
+		return new Player.MotorcyleActingOnRunway(motorcycle, newRunway(), action);
+	}
+
 	@Test
 	public void zeroZeroIsClear() throws Exception {
 		assertThat(newRunway().isClear(0, 0)).isTrue();
 	}
-	
+
 	@Test
 	public void zeroThreeIsNotClear() throws Exception {
 		assertThat(newRunway().isClear(3, 0)).isFalse();
 	}
-	
+
 	@Test
 	public void outsideYIsClear() throws Exception {
 		assertThat(newRunway().isClear(1000, 0)).isTrue();
 	}
-	
+
 	@Test
 	public void downFrom42IsClear() throws Exception {
 		assertThat(newRunway().isClear(4, 2)).isTrue();
 		assertThat(newRunway().isClear(6, 3)).isTrue();
 		assertThat(newRunway().isClear(5, 2)).isTrue();
 		assertThat(newRunway().isClear(5, 3)).isTrue();
-		assertThat(newRunway().isPathClear(4, 2, 6, 3)).isTrue();
+		Player.Motorcycle motorcycle = builderAt42().goingAt(2).build();
+		Player.Action action = Player.Action.DOWN;
+		assertThat(motorcycle.act(action).getX()).isEqualTo(6);
+		assertThat(motorcycle.act(action).getY()).isEqualTo(3);
+		assertThat(newMotorcycleActingOnRunway(motorcycle, action).isPathClear()).isTrue();
+		motorcycle = builderAt42().goingAt(3).build();
+		action = Player.Action.UP;
+		assertThat(newMotorcycleActingOnRunway(motorcycle, action).isPathClear()).isFalse();
 	}
 
+	protected Player.Motorcycle.Builder builderAt42() {
+		return new Player.Motorcycle.Builder().atX(4).atY(2);
+	}
+	
+	@Test
+	public void motorcycleCanJumpOverObstacle() throws Exception {
+		assertThat(newMotorcycleActingOnRunway(builderAt42().goingAt(3).build(), Player.Action.JUMP).isPathClear()).isTrue();
+	}
+
+	@Test
+	public void inactiveMotorcycleChangesSpeed() throws Exception {
+		Player.Motorcycle inactiveMotorcycle = new Player.Motorcycle.Builder().active(false).build();
+		assertThat(inactiveMotorcycle.act(Player.Action.SPEED).getSpeed()).isEqualTo(inactiveMotorcycle.getSpeed() + 1);
+	}
+
+	@Test
+	public void motorcycleSlowsDown() throws Exception {
+		Player.Motorcycle motorcycle = new Player.Motorcycle.Builder().goingAt(1).build();
+		assertThat(motorcycle.act(Player.Action.SLOW).getSpeed()).isEqualTo(0);
+	}
+
+	@Test
+	public void activeMotorcycleChangesXWhenSpeedIs1() throws Exception {
+		Player.Motorcycle motorcycle = new Player.Motorcycle.Builder().goingAt(1).build();
+
+		assertThat(motorcycle.act(Player.Action.WAIT).getX()).isEqualTo(motorcycle.getX() + 1);
+	}
+
+	@Test
+	public void inactiveMotorcycleDoesntChangeXWhenSpeedIs1() throws Exception {
+		Player.Motorcycle motorcycle = new Player.Motorcycle.Builder().goingAt(1).active(false).build();
+
+		assertThat(motorcycle.act(Player.Action.JUMP).getX()).isEqualTo(motorcycle.getX());
+	}
+
+	@Test
+	public void activeMotorcycleDecreasesYWhenGoingUP() throws Exception {
+		Player.Motorcycle motorcycle = new Player.Motorcycle.Builder().goingAt(1).atY(1).build();
+
+		assertThat(motorcycle.act(Player.Action.UP).getY()).isEqualTo(0);
+	}
+
+	@Test
+	public void activeMotorcycleIncreasesYWhenGoingDOWN() throws Exception {
+		Player.Motorcycle motorcycle = new Player.Motorcycle.Builder().goingAt(1).atY(1).build();
+
+		assertThat(motorcycle.act(Player.Action.DOWN).getY()).isEqualTo(2);
+	}
+	
+	@Test
+	public void goingUpNotAllowedIfMotorcycleOnTheFirstLane() throws Exception {
+		assertThat(new Player.MotorcyclesOnRunway(Collections.singleton(
+				new Player.Motorcycle.Builder().build()), newRunway(), 2).allowedActions()).doesNotContain(Player.Action.UP);
+	}
+	
+	@Test
+	public void goingUpAllowedIfMotorcycleOnTheFirstLaneIsInactive() throws Exception {
+		assertThat(new Player.MotorcyclesOnRunway(Collections.singleton(
+				new Player.Motorcycle.Builder().active(false).build()), 
+				newRunway(), 2).allowedActions()).contains(Player.Action.UP);
+	}
+	
+	@Test
+	public void goingDownNotAllowedIfMotorcycleOnTheLastLane() throws Exception {
+		assertThat(new Player.MotorcyclesOnRunway(Collections.singleton(
+				new Player.Motorcycle.Builder().atY(3).build()), newRunway(), 2).allowedActions())
+					.doesNotContain(Player.Action.DOWN);
+	}
+	
+	@Test
+	public void solvesSimpleProblem() throws Exception {
+		assertThat(new Player.SearchMotorcyclesPath(
+				Arrays.asList(new Player.Motorcycle.Builder().goingAt(1).atY(1).build(),
+						new Player.Motorcycle.Builder().goingAt(1).atY(2).build()),
+				newRunway(), 1).getActionsToGoal().get(0)).isEqualTo(Player.Action.SPEED);
+	}
+	
+	@Test
+	public void solvesHardProblem() throws Exception {
+		System.out.println(new Player.SearchMotorcyclesPath(
+				Arrays.asList(new Player.Motorcycle.Builder().goingAt(1).atY(1).build(),
+						new Player.Motorcycle.Builder().goingAt(1).atY(2).build()),
+				newRunway(heavyIn), 1).getActionsToGoal());
+	}
 }
+

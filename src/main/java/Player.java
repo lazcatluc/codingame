a4c6f631110d6af0a5beadc8a9eb0712a081f770
Lazar <LCLazar@DBDC.luxoft.com>
2016-06-10 15:47:55 +0300

    add reasonable locations

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -36,6 +35,52 @@ class Player {
             System.out.println("3 0");
         }
     }
+	
+	static class ReasonableLocationsGenerator {
+		private final BombMap bombMap;
+		private final int width;
+		private final int height;
+		
+		public ReasonableLocationsGenerator(BombMap bombMap, int width, int height) {
+			this.bombMap = bombMap;
+			this.width = width;
+			this.height = height;
+		}
+		
+		public List<Node> getReasonableLocation() {
+			Map<Node, Set<Node>> affectedAreas = new HashMap<>();
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {					
+					if (bombMap.charAt(x, y) == '.') {
+						Node node = new Node(x, y);	
+						affectedAreas.put(node, bombMap.getAffectedNodes(node));
+					}
+				}
+			}
+			Map<Node, Set<Node>> affectedAreasExcludingThoseAlreadyIncluded = new HashMap<>();
+			affectedAreas.forEach((node, set) -> { 
+				if (affectedAreasExcludingThoseAlreadyIncluded.values().stream().noneMatch(oldSet -> oldSet.containsAll(set))) {
+					removeAllIncluded(affectedAreasExcludingThoseAlreadyIncluded, set);
+					affectedAreasExcludingThoseAlreadyIncluded.put(node, set);
+				};
+			});
+			Set<Node> reasonableLocations = affectedAreasExcludingThoseAlreadyIncluded.keySet();
+			List<Node> reasonableLocationsMostAffectedNodesFirst = new ArrayList<>(reasonableLocations);
+			Collections.sort(reasonableLocationsMostAffectedNodesFirst, (node1, node2) -> 
+				affectedAreasExcludingThoseAlreadyIncluded.get(node2).size()-
+				affectedAreasExcludingThoseAlreadyIncluded.get(node1).size());
+			return reasonableLocationsMostAffectedNodesFirst;
+		}
+		
+		private void removeAllIncluded(Map<Node, Set<Node>> nodes, Set<Node> container) {
+			Iterator<Map.Entry<Node, Set<Node>>> entryIt = nodes.entrySet().iterator();
+			while (entryIt.hasNext()) {
+				if (container.containsAll(entryIt.next().getValue())) {
+					entryIt.remove();
+				}
+			}
+		}
+	}
 	
 	static class Node {
 		private final int x;
@@ -78,11 +123,9 @@ class Player {
 		private final int bombPower = 3;
 		private final List<String> map;
 		
-		
 		public BombMap(List<String> map) {
 			this.map = new ArrayList<>(map);
 		}
-
 
 		public Set<Node> getAffectedNodes(Node node) {
 			Set<Node> nodes = new HashSet<>();
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -8,21 +9,38 @@ import java.util.List;
 import org.junit.Test;
 
 public class PlayerTest {
+	private List<String> map = 
+			Arrays.asList(
+			"...@...",
+			"...#...",
+			".......",
+			".....@@",
+			".......",
+			".......",
+			".......");
+	
 	
 	@Test
-	public void generatesAffectedNodes() throws Exception {
-		List<String> map = Arrays.asList(
-				"...@...",
-				"...#...",
-				".......",
-				".....@@",
-				".......",
-				".......",
-				".......");
-		assertThat(new Player.BombMap(map).getAffectedNodes(new Player.Node(3,3))).isEqualTo(
-				new HashSet<>(Arrays.asList(new Player.Node(5,3), new Player.Node(6,3))));
-		assertThat(new Player.BombMap(map).getAffectedNodes(new Player.Node(0,0))).isEqualTo(
-				Collections.singleton(new Player.Node(3,0)));
+	public void generatesAffectedNodes33() throws Exception {
+		assertThat(newMap(map).getAffectedNodes(new Player.Node(3, 3)))
+				.isEqualTo(new HashSet<>(Arrays.asList(new Player.Node(5, 3), new Player.Node(6, 3))));
+
+	}
+
+	@Test
+	public void generatesAffectedNodes00() throws Exception {
+		assertThat(newMap(map).getAffectedNodes(new Player.Node(0, 0)))
+				.isEqualTo(Collections.singleton(new Player.Node(3, 0)));
+	}
+
+	@Test
+	public void generatesReasonableLocations() throws Exception {
+		assertThat(new Player.ReasonableLocationsGenerator(newMap(map), 7, 7).getReasonableLocation())
+				.contains(new Player.Node(6, 0), new Player.Node(5, 0));
+	}
+
+	protected Player.BombMap newMap(List<String> map) {
+		return new Player.BombMap(map);
 	}
 }
 

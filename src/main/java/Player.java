1bc17e6f68e26c1f3e842c34ebe96f1f9f8b6e7d
Lazar <LCLazar@DBDC.luxoft.com>
2016-06-24 13:16:36 +0300

    add location

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,4 +1,5 @@
-import java.	io.PrintStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,42 +17,105 @@ import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 /**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
+ * Auto-generated code below aims at helping you parse the standard input
+ * according to the problem statement.
  **/
 class Player {
-	
+
 	public static void main(String args[]) throws InterruptedException {
 		run(new MyScanner(new Scanner(System.in)), System.out);
-    }
+	}
 
 	protected static void run(MyCustomScanner in, PrintStream out) throws InterruptedException {
-		int TX = in.nextInt();
-        int TY = in.nextInt();
+		int thorX = in.nextInt();
+		int thorY = in.nextInt();
 
-        // game loop
-        while (true) {
-            int hammerStrikes = in.nextInt(); // the remaining number of hammer strikes.
-            int giants = in.nextInt(); // the number of giants which are still present on the map.
-            for (int i = 0; i < giants; i++) {
-                int giantX = in.nextInt();
-                int giantY = in.nextInt();
-            }
+		// game loop
+		while (true) {
+			int hammerStrikes = in.nextInt(); // the remaining number of hammer
+												// strikes.
+			int giants = in.nextInt(); // the number of giants which are still
+										// present on the map.
+			for (int i = 0; i < giants; i++) {
+				int giantX = in.nextInt();
+				int giantY = in.nextInt();
+			}
 
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
+			// Write an action using System.out.println()
+			// To debug: System.err.println("Debug messages...");
 
-
-            // The movement or action to be carried out: WAIT STRIKE N NE E SE S SW W or N
-            out.println("WAIT");
-        }
+			// The movement or action to be carried out: WAIT STRIKE N NE E SE S
+			// SW W or N
+			out.println("WAIT");
+		}
 	}
-	
+
+	static class Location {
+		public static final Location BOTTOM_RIGHT = new Location(39, 17);
+
+		private final int x;
+		private final int y;
+
+		public Location(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + x;
+			result = prime * result + y;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Location other = (Location) obj;
+			if (x != other.x)
+				return false;
+			if (y != other.y)
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "(" + x + "," + y + ")";
+		}
+
+		public Set<Location> getNeighbors() {
+			Set<Location> ret = new HashSet<>();
+			for (int x = this.x - 1; x < this.x + 2; x++) {
+				for (int y = this.y - 1; y < this.y + 2; y++) {
+					if (x == this.x && y == this.y) {
+						continue;
+					}
+					if (x < 0 || y < 0 || x > BOTTOM_RIGHT.x || y > BOTTOM_RIGHT.y) {
+						continue;
+					}
+					ret.add(new Location(x, y));
+				}
+				
+			}
+			return ret;
+		}
+
+	}
+
 	interface MyCustomScanner {
 		String nextLine();
+
 		int nextInt();
 	}
-	
+
 	static class MyScanner implements MyCustomScanner {
 		private final Scanner scanner;
 		private final List<String> lines = new ArrayList<>();
@@ -76,17 +140,16 @@ class Player {
 		public String toString() {
 			StringBuilder sb = new StringBuilder();
 			appendLine(0, sb);
-			for(int i = 1; i < lines.size(); i++) {
+			for (int i = 1; i < lines.size(); i++) {
 				sb.append(",\n");
 				appendLine(i, sb);
 			}
 			return sb.toString();
 		}
-		
+
 		private void appendLine(int line, StringBuilder sb) {
 			sb.append("\"").append(lines.get(line).replaceAll("\"", "\\\"")).append("\"");
 		}
 	}
-	
-}
 
+}
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -16,7 +17,15 @@ import org.junit.Test;
 
 public class PlayerTest {
 	
+	@Test
+	public void location11has8Neighbors() throws Exception {
+		assertThat(new Player.Location(1, 1).getNeighbors().size()).isEqualTo(8);
+	}
 	
+	@Test
+	public void location01Has5Neighbors() throws Exception {
+		assertThat(new Player.Location(0, 1).getNeighbors().size()).isEqualTo(5);
+	}
 }
 
 

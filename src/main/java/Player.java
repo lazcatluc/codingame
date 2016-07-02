c9464230f00f14922bca058f42295bf9c0498f8a
Catalin Lazar <lazcatluc@gmail.com>
2016-07-02 16:29:38 +0300

    finished overlapper

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -9,16 +9,93 @@ class Player {
 
     public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		for (int i = 0; i < N; i++) {
-			String subseq = in.next();
+		int sequencesCount = in.nextInt();
+		Set<String> sequences = new HashSet<>();
+		for (int i = 0; i < sequencesCount; i++) {
+			sequences.add(in.next());
 		}
 
 		// Write an action using System.out.println()
 		// To debug: System.err.println("Debug messages...");
 
-		System.out.println("answer");
+		System.out.println(getSmallestSequenceLength(sequences));
     }
 
+	public static int getSmallestSequenceLength(Set<String> sequences) {
+		OverlapperCache cache = new OverlapperCache();
+		int maxOverlap = generatePermutations(sequences).stream().map(list -> getOverlapCount(list, cache))
+				.max(Integer::compare).get();
+		int lengthSum = sequences.stream().map(String::length).reduce(0, Integer::sum);
+
+		return lengthSum - maxOverlap;
+	}
+
+	public static int getOverlapCount(List<String> sequence, OverlapperCache cache) {
+		int overlapCount = 0;
+		for (int i = 0; i < sequence.size() - 1; i++) {
+			overlapCount += cache.get(sequence.get(i), sequence.get(i + 1)).getOverlapCount();
+		}
+		return overlapCount;
+	}
+
+	public static Set<List<String>> generatePermutations(Set<String> originalSet) {
+		if (originalSet.isEmpty()) {
+			return Collections.singleton(new ArrayList<>());
+		}
+		return originalSet.stream().flatMap(element -> {
+			Set<String> withoutElement = new HashSet<>(originalSet);
+			withoutElement.remove(element);
+			Set<List<String>> permutations = generatePermutations(withoutElement);
+			permutations.forEach(list -> list.add(element));
+			return permutations.stream();
+		}).collect(Collectors.toSet());
+	}
+
+	static class OverlapperCache {
+		private Map<String, Map<String, Overlapper>> overlappers = new HashMap<>();
+
+		public Overlapper get(String first, String second) {
+			Map<String, Overlapper> cache = overlappers.get(first);
+			if (cache == null) {
+				cache = new HashMap<>();
+				overlappers.put(first, cache);
+			}
+			Overlapper overlapper = cache.get(second);
+			if (overlapper == null) {
+				overlapper = new Overlapper(first, second);
+				cache.put(second, overlapper);
+			}
+			return overlapper;
+		}
+	}
+
+	static class Overlapper {
+		private final String first;
+		private final String second;
+		private final int overlapCount;
+
+		private Overlapper(String first, String second) {
+			this.first = first;
+			this.second = second;
+			this.overlapCount = overlapCount();
+		}
+
+		public int getOverlapCount() {
+			return overlapCount;
+		}
+
+		private int overlapCount() {
+			for (int i = first.length(); i > 0; i--) {
+				String substring = first.substring(first.length() - i, first.length());
+				if (second.startsWith(substring)) {
+					return i;
+				}
+				if (substring.startsWith(second)) {
+					return second.length();
+				}
+			}
+			return 0;
+		}
+	}
 
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -2,17 +2,53 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayInputStream;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 import org.junit.After;
 import org.junit.Test;
 
 public class PlayerTest {
-	
 
+	@Test
+	public void gattaOverlap() throws Exception {
+		assertThat(new Player.OverlapperCache().get("AGATTA", "GATTACA").getOverlapCount()).isEqualTo(5);
+
+	}
+
+	@Test
+	public void generatesPermutations() throws Exception {
+		Set<String> set = new HashSet<>();
+		set.add("1");
+		set.add("2");
+		set.add("3");
+		Set<List<String>> permutations = new HashSet<>();
+		permutations.add(Arrays.asList("1", "2", "3"));
+		permutations.add(Arrays.asList("1", "3", "2"));
+		permutations.add(Arrays.asList("2", "1", "3"));
+		permutations.add(Arrays.asList("2", "3", "1"));
+		permutations.add(Arrays.asList("3", "1", "2"));
+		permutations.add(Arrays.asList("3", "2", "1"));
+		assertThat(Player.generatePermutations(set)).isEqualTo(permutations);
+
+	}
+
+	@Test
+	public void getsSmallestSequence() throws Exception {
+		Set<String> set = new HashSet<>();
+		set.add("AGATTA");
+		set.add("GATTACA");
+		set.add("TACAGA");
+
+		assertThat(Player.getSmallestSequenceLength(set)).isEqualTo(10);
+	}
+
+	@Test
+	public void computesAggregatedOverlapCount() throws Exception {
+		List<String> list = new ArrayList<>();
+		list.add("AGATTA");
+		list.add("GATTACA");
+		list.add("TACAGA");
+
+		assertThat(Player.getOverlapCount(list, new Player.OverlapperCache())).isEqualTo(9);
+	}
 }

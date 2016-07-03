5e4009a755833c18ebe5fcc1c03a308e2c3a8c1b
Catalin Lazar <lazcatluc@gmail.com>
2016-07-03 10:21:03 +0300

    initial commit

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -2,17 +2,11 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayInputStream;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 import org.junit.After;
 import org.junit.Test;
 
 public class PlayerTest {
-	
 
 }
